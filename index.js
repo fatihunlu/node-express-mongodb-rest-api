da38d9d1e06668e95bcfa313f29c4731f98f7c69
Fatih Ünlü <unlu.fa@gmail.com>
2022-05-30 23:07:03 +0300

    inital commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv");
+const express = require("express");
+const app = express();
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my tutorial" });
+});
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
